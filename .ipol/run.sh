f91c4b822bf89619209287ed9633bf2189df43ea
luciabouza <96236370+luciabouzaheguerte@users.noreply.github.com>
2022-09-19 13:29:56 +0200

    .ipol

---
+++ .ipol/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+task=${1}
+model=${2}
+segmentation=${3}
+
+echo ./image_abstraction input_0.png $task $model $segmentation input_1.png
+
+./image_abstraction input_0.png $task $model $segmentation input_1.png
