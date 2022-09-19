b4fde30cf82293d50117a920e5a8a01f3bb38ae8
luciabouzaheguerte <96236370+luciabouzaheguerte@users.noreply.github.com>
2022-09-19 17:20:37 +0200

    Update run.sh

--- .ipol/run.sh
+++ .ipol/run.sh
@@ -5,8 +5,6 @@ task=${1}
 model=${2}
 segmentation=${3}
 
-echo ./image_abstraction input_0.png $task $model $segmentation input_1.png
+echo $bin/image_abstraction input_0.png $task $model $segmentation input_1.png
 
-cp /workdir/bin/image_abstraction /workdir/exec/image_abstraction
-
-./image_abstraction input_0.png $task $model $segmentation input_1.png
+$bin/image_abstraction input_0.png $task $model $segmentation input_1.png
