d6c4ce901ef64d184ab915c0357924307970a390
luciabouza <lucia.bouza-heguerte@u-paris.fr>
2022-11-03 12:49:09 -0300

    Update run.sh

--- .ipol/run.sh
+++ .ipol/run.sh
@@ -17,8 +17,6 @@ scaleratio=${13}
 threshold=${14}
 eps=${15}
 
-convert mask_0.png -white-threshold 000001 -alpha off mask_0.png
-
 echo $bin/image_abstraction input_0.png $task $model $options $color_sketch $renderOrder $alpha $modelDictionary $mcolor $equal $kappaDict $mpixel $maxarea $scaleratio $threshold $eps input_1.png mask_0.png
 
 $bin/image_abstraction input_0.png $task $model $options  $color_sketch $renderOrder $alpha $modelDictionary $mcolor $equal $kappaDict $mpixel $maxarea $scaleratio $threshold $eps input_1.png mask_0.png
