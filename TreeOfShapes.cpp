b9f9d238e0d27554b9aed577bbe98cee866eaa7d
luciabouza <lucia.bouza-heguerte@u-paris.fr>
2022-11-10 11:09:42 -0300

    Update TreeOfShapes.cpp

--- TreeOfShapes.cpp
+++ TreeOfShapes.cpp
@@ -3065,7 +3065,7 @@ QImage TreeOfShapes::render(TOSParameters tosParameters, bool &tree_recomputed, 
             */
 
             synshapeRect(pShape, imgsyn, &ALPHA, &tosParameters.relief, &tosParameters.reliefOrientation, &tosParameters.reliefHeight);          
-
+            /*
             if (_len_ArrayPixelsMask != 0){
                 pCurrentPoint = &_ArrayPixelsMask[0];
                 color_mask =image_mask.pixel( pCurrentPoint->x, pCurrentPoint->y ); 
@@ -3080,6 +3080,7 @@ QImage TreeOfShapes::render(TOSParameters tosParameters, bool &tree_recomputed, 
                         }
                     }
             }
+            */
             
         
         } 
