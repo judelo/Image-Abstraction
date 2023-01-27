f667523cfa082eadee36320b8d6ccd5fa29ebe25
luciabouza <lucia.bouza-heguerte@u-paris.fr>
2023-01-27 12:48:13 +0100

    remove condition Dist*CONT <0 (always positive)

--- TreeOfShapes.cpp
+++ TreeOfShapes.cpp
@@ -1145,10 +1145,10 @@ void TreeOfShapes::filter_image(int *ns,float *threshold,float *minarea,float *m
 
         // Conditions to remove shape
         // Area smaller than mpixel or larger than maxpixel or
-        // (area/area_parent)*Contrast between both is less than threshold or
-        // distance multiply by contrast between shape and parent is negative. How can be negative?? or 
-        // (area/area_parent) smaller than compactess parameter. // How can be negative?? k it is set to zero for all tasks. 
-        if(pShape->area <= mpixel || maxpixel < pShape->area || (((Info*)(pShape->data))->attribute[0])*CONTR<= thre || Dist*CONTR < 0. || ((((Info*)(pShape->data))->attribute[0])< (*k)))
+        // (area/area_grand_parent)*(Contrast between shape and parent) is less than threshold or
+        // distance multiply by contrast between shape and parent is negative. How can be negative??
+        //removed: || Dist*CONTR < 0.
+        if(pShape->area <= mpixel || maxpixel < pShape->area || (((Info*)(pShape->data))->attribute[0])*CONTR<= thre || ((((Info*)(pShape->data))->attribute[0])< (*k)))
             pShape->removed = 1;
         else
             pShape->removed = 0;
@@ -1245,9 +1245,8 @@ Shape TreeOfShapes::selectShapeDict(Shape pShape,
     minDist = 10000.0;
     
     if(*randS == 0){ // randS=0, randomly select shapes; 
-        //temp = _pTree->nb_shapes -1;
-        //index = random_number(&temp) + 1;
-        index = ((((int)rand())/RAND_MAX) % (_pTree->nb_shapes -1)) +1;
+        temp = _pTree->nb_shapes -1;
+        index = random_number(&temp) + 1;
     } else{
         // randS=1, select shapes according to elongation, compactness and scale;  
         // randS=2, select shapes according to elongation, compactness, scale and color
